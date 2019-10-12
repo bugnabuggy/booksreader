947c0f9e7271903524d3c7cc5d773f73d77835a8
Anton Yakovets <jokerosky@gmail.com>
2019-10-12 14:01:23 +0600

    api url in frontend prod config

--- src/BooksReader.Web/ClientApp/src/environments/environment.prod.ts
+++ src/BooksReader.Web/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseApiUrl: '/',
+  baseApiUrl: 'https://br.ruteco.com/',
   facebookAppId: '277711532267100',
   tinyMCEApiKey:'rn8afmd09o68emfvblb3yifrdm28g4tfaar7dj1je3z9p0fk'
 };
